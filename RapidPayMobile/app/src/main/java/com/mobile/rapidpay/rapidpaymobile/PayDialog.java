f8ed3056d9b28aa57a8325c10c884f5cb10910ed
mseyirci <mehmet.seyirci@gmail.com>
2017-05-28 14:11:47 +0300

    pay dialog

---
+++ RapidPayMobile/app/src/main/java/com/mobile/rapidpay/rapidpaymobile/PayDialog.java
@@ -0,0 +1,48 @@
+package com.mobile.rapidpay.rapidpaymobile;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+public class PayDialog  extends Dialog implements
+        android.view.View.OnClickListener {
+
+    public Activity c;
+    public Dialog d;
+    public Button yes, no;
+
+    public PayDialog(Activity a) {
+        super(a);
+        this.c = a;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.pay_dialog);
+        yes = (Button) findViewById(R.id.btn_yes);
+        no = (Button) findViewById(R.id.btn_no);
+        yes.setOnClickListener(this);
+        no.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_yes:
+                c.finish();
+                break;
+            case R.id.btn_no:
+                dismiss();
+                break;
+            default:
+                break;
+        }
+        dismiss();
+    }
+}
+
